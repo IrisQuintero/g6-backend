99d307115d6d34b4251e2222876e3549934ee066
Iris Quintero <qupi_44@hotmail.com>
2021-08-09 19:54:51 -0500

    backend

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const app = require('./src/server');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.listen(3000, () => console.log('Local app listening on port 3000!'));
