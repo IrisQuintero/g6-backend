0d5a95fe7d00bd42cb6e69ae442cd8a9042d86c2
Iris Quintero <qupi_44@hotmail.com>
2021-08-09 20:44:26 -0500

    backend2

--- src/server.js
+++ src/server.js
@@ -1,13 +1,19 @@
+//Importamos los paquetes
 const express = require('express');
 const path = require('path');
 const serverless = require('serverless-http');
 const bodyParser = require('body-parser');
 
+// simulamos la base de datos
+let productos = [{name: "papitas"},{name:"agua"}];
 
+//Generamos la app 
 const app = express();
 
+//Definición de rutas o endpoints 
 const router = express.Router();
 router.get('/', (req, res) => res.send({"mensaje": "hola"}));
+router.get('/productos', (req, res) => res.send(productos));
 
 
 app.use('/.netlify/functions/server', router);  // path must route to lambda
@@ -15,3 +21,8 @@ app.use('/.netlify/functions/server', router);  // path must route to lambda
 
 module.exports = app;
 module.exports.handler = serverless(app);
+
+// netlify comands; 
+//npm install & npm run (build comand)
+//dist(publish directory)
+
